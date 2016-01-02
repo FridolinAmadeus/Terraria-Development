66794b51d7955e136e1d3a17f4fbf6688242132c
Foltermister <Alexey.Riepenhausen@gmx.de>
2016-01-02 23:37:03 +0100

    schluss fuer heute

--- leltestmod/Projectiles/Railgunbullet.cs
+++ leltestmod/Projectiles/Railgunbullet.cs
@@ -16,9 +16,9 @@ public class Railgunbullet : ModProjectile
         projectile.aiStyle = 1;
         projectile.friendly = true;
         projectile.ranged = true;
-        projectile.penetrate = 5;
+        projectile.penetrate = 1;
         projectile.timeLeft = 600;
-        projectile.light = 0.5f;
+        //projectile.light = 0.5f;
         projectile.extraUpdates = 1;
         //ProjectileID.Sets.TrailCacheLength[projectile.type] = 5;
         //ProjectileID.Sets.TrailingMode[projectile.type] = 0;
